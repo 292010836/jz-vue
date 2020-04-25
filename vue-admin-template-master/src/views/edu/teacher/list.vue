89b66329367187e9d238f8f27becae66a0ac9281
jz1yer <2920123@qq.com>
2020-04-25 16:36:40 +0800

    第一次提交

---
+++ vue-admin-template-master/src/views/edu/teacher/list.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="app-container">
+        讲师列表
+    </div>
+</template>
+
+<<script>
+import teacher from '@/api/edu/teacher'
+
+export default {
+    data() {
+        return {
+            list:null,
+            page:1,
+            limit:10,
+            total:0,
+            teacherQuery:{}
+        }
+    },
+    created() {
+        this.getList();
+    },
+    methods: {
+        getList(){
+            teacher.getTeacherListPage(this.page,this.limit,this.teacherQuery)
+            .then(response=>{
+                console.log(response);
+            })
+            .catch(error=>{
+                console.log(error);
+                
+            })
+        }
+    },
+
+}
+</script>
---
+++ vue-admin-template-master/src/views/edu/teacher/save.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="app-container">
+        保存讲师
+    </div>
+</template>
