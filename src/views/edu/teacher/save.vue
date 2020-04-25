98bb856a0ff1be519c60c33c51439e91d53c620b
jz1yer <2920123@qq.com>
2020-04-26 01:17:46 +0800

    在线教育前端代码

--- src/views/edu/teacher/save.vue
+++ src/views/edu/teacher/save.vue
@@ -1,5 +1,58 @@
 <template>
     <div class="app-container">
-        保存讲师1
+      <el-form label-width="120px">
+        <el-form-item label="讲师名称">
+          <el-input v-model="teacher.name"/>
+        </el-form-item>
+        <el-form-item label="讲师排序">
+          <el-input-number v-model="teacher.sort" controls-position="right" min="0"/>
+        </el-form-item>
+        <el-form-item label="讲师头衔">
+          <el-select v-model="teacher.level" clearable placeholder="请选择">
+            <el-option :value="1" label="高级讲师"/>
+            <el-option :value="2" label="首席讲师"/>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="讲师资历">
+          <el-input v-model="teacher.career"/>
+        </el-form-item>
+        <el-form-item label="讲师简介">
+          <el-input v-model="teacher.intro" :rows="10" type="textarea"/>
+        </el-form-item>
+
+        <!-- 讲师头像：TODO -->
+
+        <el-form-item>
+          <el-button :disabled="saveBtnDisabled" type="primary" @click="saveOrUpdate">保存</el-button>
+        </el-form-item>
+      </el-form>
     </div>
 </template>
+<script>
+  import teacher from '@/api/edu/teacher'
+    export default {
+      data(){
+        return{
+            teacher : {},
+        }
+      },
+
+      created(){
+
+      },
+
+      methods:{
+        saveOrUpdate(){
+            teacher.addTeacher(this.teacher)
+              .then(response=>{
+                this.$message({
+                  type: 'success',
+                  message: '保存成功!'
+                })
+              })
+        }
+
+      }
+
+    }
+</script>
